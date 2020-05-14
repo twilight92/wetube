2163fcf231270ba81129908bac54c7f43b971eef
tnwjd2050 <tnwjd2050@naver.com>
2020-05-14 22:57:05 +0900

    body로부터 특정한 정보를 얻게 해주는 body-parser/session을 다루려 cookie에 유저 정보 저장하기 위한 cookie-parser 미들웨어 install

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 import "core-js";
 const app = express();
 
@@ -12,6 +14,9 @@ const handleHome = (req, res) => res.send("Hello from home");
 
 const handleProfile = (req, res) => res.send("Hello from profile");
 
+app.use(cookieParser());
+app.use(bodyParser());
+app.use(helmet());
 app.use(morgan("dev"))
 
 app.get("/", handleHome);
