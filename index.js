3df97988041e625d4987569566c2e8a50e903411
tnwjd2050 <tnwjd2050@naver.com>
2020-05-14 23:00:29 +0900

    body-parser 옵션 추가/ form에서 받은 데이터 뿐만 아니라(urlencoded) json도 이해하길 바람

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ const handleHome = (req, res) => res.send("Hello from home");
 const handleProfile = (req, res) => res.send("Hello from profile");
 
 app.use(cookieParser());
-app.use(bodyParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"))
 
