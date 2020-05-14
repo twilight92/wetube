a705a0b32d5b0ddc58e621fffb278be0f52c6bf3
tnwjd2050 <tnwjd2050@naver.com>
2020-05-14 22:32:13 +0900

    dependencies 추가/ log를 위해 미들웨어 morgan install

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("HI")
+import express from "express";
+import morgan from "morgan";
+import "core-js";
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send("Hello from home");
+
+const handleProfile = (req, res) => res.send("Hello from profile");
+
+app.use(morgan("dev"))
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
