848a8513b12c5484a76810d79abaa99def352754
tnwjd2050 <tnwjd2050@naver.com>
2020-05-16 19:03:47 +0900

    템플릿마다 다른 정보를 가질수도 있기 때문에 템플릿 각각에 정보를 전달하는 방법 추가

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,8 @@
-export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.send("Search");
-export const videos = (req, res) => res.send("Videos");
-export const upload = (req, res) => res.send("Upload");
-export const videoDetail = (req, res) => res.send("Video Detail");
-export const editVideo = (req, res) => res.send("Edit Video");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+// render 함수의 첫번째 인자는 템플릿, 두번째 인자는 템플릿에 추가할 정보가 담긴 객체
+export const search = (req, res) => res.render("search", { pageTitle: "Search" });
+export const videos = (req, res) => res.render("videos", { pageTitle: "Videos" });
+export const upload = (req, res) => res.render("upload", { pageTitle: "Upload" });
+export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });
+export const editVideo = (req, res) => res.render("editVideo", { pageTitle: "Edit Video" });
+export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Video" });
