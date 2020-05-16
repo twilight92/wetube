730fa940d4152043beeec7e13e1e5cda4d273186
tnwjd2050 <tnwjd2050@naver.com>
2020-05-16 22:40:25 +0900

    Join Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,23 @@
-export const join = (req, res) => res.render("join", { pageTitle: "Join" });
+import routes from "../routes";
+
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+export const postJoin = (req, res) => {
+    // console.log(req.body)
+    const {
+        body: { name, email, password, password2}
+    } = req;
+    if (password !== password2) {
+        // 400(잘못된 요청 - bad request) 상태코드 전달
+        res.status(400);
+
+        // res에서도 가능하다. 마지막에 res.render를 한다면, 잘못된 요청이라는 상태코드로 화면을 표시한다.
+        res.render("join", { pageTitle: "Join" });
+    } else {
+        // To Do: Resister User
+        // To Do: Log user in
+        res.redirect(routes.home);
+    }
+}
 export const login = (req, res) => res.render("login", { pageTitle: "Login" });
 export const logout = (req, res) => res.render("logout", { pageTitle: "Logout" });
 export const users = (req, res) => res.render("users", { pageTitle: "Users" });
