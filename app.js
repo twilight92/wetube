bc2f679c254027855de15a2bb606588c0b588959
tnwjd2050 <tnwjd2050@naver.com>
2020-05-16 10:17:43 +0900

    user에 관련된 userRouter, video에 관련된 videoRouter, join/home/login 등을 다룰 globalRouter 생성

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
+import { userRouter } from "./routers/userRouter";
+import { videoRouter } from "./router/videoRouter";
+import { globalRouter } from "./router/globalRouter";
 // default로 export하지 않았기 때문에 이렇게 import
 
 const app = express();
@@ -18,11 +20,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
+app.use("/", globalRouter);
+app.use("/user", userRouter);
+app.use("/video", videoRouter);
 
-app.get("/profile", handleProfile);
-
-app.use("/user", userRouter)
 //use의 의미: 누군가 /user 경로에 접속하면 이 router 전체를 사용하겠다.
 
 export default app;
