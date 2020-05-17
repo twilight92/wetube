b0d3af767499426d185279986672f5163912068a
tnwjd2050 <tnwjd2050@naver.com>
2020-05-17 17:41:36 +0900

    edit video get,post 분리/ import 순서 알파벳 순으로 변경/ 주석 추가

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -2,19 +2,28 @@ import express from "express";
 import routes from "../routes";
 import {
     postUpload,
+    postEditVideo,
+    deleteVideo,
     getUpload,
-    videoDetail,
-    editVideo,
-    deleteVideo
+    getEditVideo,
+    videoDetail
 } from "../controllers/videoController";
 import { uploadVideo } from "../middlewares";
 
 export const videoRouter = express.Router();
 
+// Upload
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
+
+// Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
+
+// Edit Video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+// Delete Video
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter;
